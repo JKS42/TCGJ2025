08230af0f017f189ddf8f90c2f8a4ece2c815d18
JKS42 <kian.jsmit@gmail.com>
2025-07-11 22:28:10 +0200

    Update
    MainMenu script start, Enemy move and go towards and don't go through walls.
    Enemy Spawns
    Enemy Attack
    Pickups |
    side effects

--- My project/Assets/Scripts/EnemyScripts/EnemyDrop.cs
+++ My project/Assets/Scripts/EnemyScripts/EnemyDrop.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class EnemySplines : MonoBehaviour
+public class EnemyDrop : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
---
+++ My project/Assets/Scripts/EnemyScripts/EnemyMovement.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform target;
+    public float moveSpeed;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+        transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+    }
+}
